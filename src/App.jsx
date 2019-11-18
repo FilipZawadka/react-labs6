dfca79149fe1d364a57464fea3b1ec5bbb53472a
FilipZawadka <38583399+FilipZawadka@users.noreply.github.com>
2019-11-18 16:48:27 +0100

    Lab6Ex1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
 
-const App = () => (
-  <h1>Minimal React</h1>  
-)
 
-export default App
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+  this.state={
+    employees: null,
+    isLoading:true
+  }
+}
+
+omponentDidMount(){
+  fetch('http://localhost:3004//employees')
+  .then(respones => response.json())
+  .then(data =>this.setState({employees:data}))
+  .then(()=>this.setState({isLoading:false}));  
+
+}
+  render(){
+    return(
+  <div>
+  {this.state.isLoading ? "Loading...":"data loaded"}
+  </div>  
+
+)
+}
+}
+export default App 
+
+
