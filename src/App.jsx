aff55b63f835adc3538cf404f04529e25bc75796
FilipZawadka <38583399+FilipZawadka@users.noreply.github.com>
2019-11-19 16:57:57 +0100

    Lab1Ex1 - Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
-import Component from './Component'
-const App = () => (
-  <div>
-    <h1>Minimal React</h1>
-    <Component />
-  </div>  
-)
+class App extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        employees: [],
+        isLoading: true,
+
+      };
+
+    }
+  
+    componentDidMount() {
+      this.setState({ isLoading:true});
+      fetch('http://localhost:3004/employees')
+      .then(response => response.json())
+      .then(data => this.setState({ employees:data }))
+      .then(() => {this.setState({ isLoading: false })
+      });
+    }
+
+  
+    render()
+    {
+      return(
+        <div>
+            {this.state.isLoading ? <label>Loading...</label>:<label>Loaded {this.state.employees.length} elements: </label>}
+            <p>
+              {this.state.employees.map((emp) => <ul key={emp.id}>{ emp.name} {emp.age} {emp.company}  {emp.email} {emp.isActive ? "Active" : "Not Active"}</ul>)}
+            </p>
+            
+
+        </div>
+      )
+    }
+}
 
 export default App
